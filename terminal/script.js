0802b9ae01c87119f5206df5974922d3e64a48a1
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-12 13:04:17 -0500

    Update script.js

--- terminal/script.js
+++ terminal/script.js
@@ -1,144 +1,146 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const terminalOutput = document.getElementById('terminal-output');
-  const commandInput = document.getElementById('command-input');
-  const commandForm = document.getElementById('command-form');
+const terminalOutput = document.getElementById('terminal-output');
+const commandInput = document.getElementById('command-input');
+const commandForm = document.getElementById('command-form');
 
-  commandForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+commandForm.addEventListener('submit', function(event) {
+event.preventDefault(); // Prevent default form submission
+  const command = commandInput.value.trim();
+executeCommand(command);
 
-    const command = commandInput.value.trim();
-    executeCommand(command);
+commandInput.value = ''; // Clear the command input after execution
+});
 
-    commandInput.value = ''; // Clear the command input after execution
-  });
+const availableCommands = [
+{
+command: 'ls',
+description: 'List files and directories',
+},
+{
+command: 'cd [directory]',
+description: 'Change directory',
+},
+{
+command: 'mkdir [directory]',
+description: 'Create a new directory',
+},
+{
+command: 'touch [file]',
+description: 'Create a new file',
+},
+{
+command: 'rm [file]',
+description: 'Delete a file',
+},
+{
+command: 'pwd',
+description: 'Show current directory',
+},
+{
+command: 'echo [message]',
+description: 'Display a message',
+},
+{
+command: 'cat [file]',
+description: 'Display file content',
+},
+{
+command: 'echo-date',
+description: 'Show current date',
+},
+{
+command: 'help',
+description: 'Show available commands',
+},
+{
+command: 'ip',
+description: 'Show public IP address',
+},
+{
+command: 'clear',
+description: 'Clear the terminal',
+},
+{
+command: 'sudo [command]',
+description: 'Run a command with administrative privileges',
+},
+{
+command: 'top',
+description: 'Display system resource usage',
+},
+{
+command: 'exit',
+description: 'Terminate the terminal',
+},
+{
+command: 'sl',
+description: 'Not available',
+}
+];
 
-  const availableCommands = [
-    {
-      command: 'ls',
-      description: 'List files and directories',
-    },
-    {
-      command: 'cd [directory]',
-      description: 'Change directory',
-    },
-    {
-      command: 'mkdir [directory]',
-      description: 'Create a new directory',
-    },
-    {
-      command: 'touch [file]',
-      description: 'Create a new file',
-    },
-    {
-      command: 'rm [file]',
-      description: 'Delete a file',
-    },
-    {
-      command: 'pwd',
-      description: 'Show current directory',
-    },
-    {
-      command: 'echo [message]',
-      description: 'Display a message',
-    },
-    {
-      command: 'cat [file]',
-      description: 'Display file content',
-    },
-    {
-      command: 'echo-date',
-      description: 'Show current date',
-    },
-    {
-      command: 'help',
-      description: 'Show available commands',
-    },
-    {
-      command: 'ip',
-      description: 'Show public IP address',
-    },
-    {
-      command: 'clear',
-      description: 'Clear the terminal',
-    },
-    {
-      command: 'sudo [command]',
-      description: 'Run a command with administrative privileges',
-    },
-    {
-      command: 'top',
-      description: 'Display system resource usage',
-    },
-    {
-      command: 'exit',
-      description: 'Exit the terminal',
-    },
-    {
-      command: 'sl',
-      description: 'Run the "sl" command for fun',
-    },
-  ];
+function getFileContent(file) {
+// Implement logic to read and return the content of the file
+// Replace with your own implementation
+return 'File content of ' + file;
+}
 
-  function getFileContent(file) {
-    // Implement logic to read and return the content of the file
-    // Replace with your own implementation
-    return 'File content of ' + file;
-  }
+function displayCommandOutput(output) {
+const outputElement = document.createElement('p');
+outputElement.classList.add('command-output');
+outputElement.textContent = output;
+terminalOutput.appendChild(outputElement);
+}
 
-  function displayCommandOutput(output) {
-    const outputElement = document.createElement('p');
-    outputElement.classList.add('command-output');
-    outputElement.textContent = output;
-    terminalOutput.appendChild(outputElement);
-  }
+function displayHelp() {
+const helpOutput = document.createElement('p');
+helpOutput.classList.add('command-output');
+helpOutput.innerHTML = '<strong>Available commands:</strong><br>';
+availableCommands.forEach(function(cmd) {
+helpOutput.innerHTML += <strong>${cmd.command}</strong>: ${cmd.description}<br>;
+});
+terminalOutput.appendChild(helpOutput);
+}
 
-  function displayHelp() {
-    const helpOutput = document.createElement('p');
-    helpOutput.classList.add('command-output');
-    helpOutput.innerHTML = '<strong>Available commands:</strong><br>';
-    availableCommands.forEach(function(cmd) {
-      helpOutput.innerHTML += `<strong>${cmd.command}</strong>: ${cmd.description}<br>`;
-    });
-    terminalOutput.appendChild(helpOutput);
-  }
+function executeCommand(command) {
+const args = command.split(' ');
+  switch (args[0]) {
+  case 'ls':
+    displayCommandOutput('File1.txt  File2.txt  Directory1');
+    break;
 
-  function executeCommand(command) {
-    const args = command.split(' ');
+  case 'cd':
+    if (args.length > 1) {
+      // Implementation for 'cd' command
+      const directory = args[1];
+      displayCommandOutput(`Changed directory to ${directory}`);
+    } else {
+      displayCommandOutput('Missing directory argument');
+    }
+    break;
 
-    switch (args[0]) {
-      case 'ls':
-        displayCommandOutput('File1.txt  File2.txt  Directory1');
-        break;
+  case 'mkdir':
+    if (args.length > 1) {
+      // Implementation for 'mkdir' command
+      const directory = args[1];
+      displayCommandOutput(`Created directory: ${directory}`);
+    } else {
+      displayCommandOutput('Missing directory name');
+    }
+    break;
 
-      case 'cd':
-        if (args.length > 1) {
-          const directory = args[1];
-          displayCommandOutput(`Changed directory to ${directory}`);
-        } else {
-          displayCommandOutput('Missing directory argument');
-        }
-        break;
-
-      case 'mkdir':
-        if (args.length > 1) {
-          const directory = args[1];
-          displayCommandOutput(`Created directory: ${directory}`);
-        } else {
-          displayCommandOutput('Missing directory name');
-        }
-        break;
-
-      case 'touch':
-        if (args
-.length > 1) {
-const file = args[1];
+  case 'touch':
+    if (args.length > 1) {
+      // Implementation for 'touch' command
+      const file
+= args[1];
 displayCommandOutput(Created file: ${file});
 } else {
 displayCommandOutput('Missing file name');
 }
 break;
-      case 'rm':
+    case 'rm':
     if (args.length > 1) {
+      // Implementation for 'rm' command
       const file = args[1];
       displayCommandOutput(`Deleted file: ${file}`);
     } else {
@@ -162,6 +164,7 @@ break;
   case 'cat':
     if (args.length > 1) {
       const fileToRead = args[1];
+      // Implement logic to read and display the content of the file
       const fileContent = getFileContent(fileToRead); // Replace with the actual file reading logic
       displayCommandOutput(fileContent);
     } else {
@@ -204,16 +207,16 @@ break;
     break;
 
   case 'top':
-    displayCommandOutput('CPU usage: 10%, Memory usage: 50%');
+    displayCommandOutput('System resource usage:');
+    // Implement logic to display system resource usage
     break;
 
   case 'exit':
-    // Handle exit logic here (e.g., close the terminal window)
-    displayCommandOutput('Exiting the terminal...');
+    // Terminate the terminal
     break;
 
   case 'sl':
-    displayCommandOutput('Sorry, the "sl" command is not available. Try something else!');
+    displayCommandOutput('Command not available');
     break;
 
   default:
