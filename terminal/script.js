4cc2b3bbdb55295cc847d7f6b046f8efb1a70f1b
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-12 09:36:02 -0500

    Update script.js

--- terminal/script.js
+++ terminal/script.js
@@ -18,90 +18,78 @@ function executeCommand(command) {
   switch (args[0]) {
     case 'ls':
       // Implement logic to display a list of files and directories
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = 'File1.txt  File2.txt  Directory1';
-      terminalOutput.appendChild(output);
+      const outputLs = document.createElement('p');
+      outputLs.classList.add('command-output');
+      outputLs.textContent = 'File1.txt  File2.txt  Directory1';
+      terminalOutput.appendChild(outputLs);
       break;
 
     case 'cd':
       // Implement logic to change the current directory
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = 'Changed directory to ' + args[1];
-      terminalOutput.appendChild(output);
+      const outputCd = document.createElement('p');
+      outputCd.classList.add('command-output');
+      outputCd.textContent = 'Changed directory to ' + args[1];
+      terminalOutput.appendChild(outputCd);
       break;
 
     case 'mkdir':
       // Implement logic to create a new directory
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = 'Created directory: ' + args[1];
-      terminalOutput.appendChild(output);
+      const outputMkdir = document.createElement('p');
+      outputMkdir.classList.add('command-output');
+      outputMkdir.textContent = 'Created directory: ' + args[1];
+      terminalOutput.appendChild(outputMkdir);
       break;
 
     case 'touch':
       // Implement logic to create a new file
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = 'Created file: ' + args[1];
-      terminalOutput.appendChild(output);
+      const outputTouch = document.createElement('p');
+      outputTouch.classList.add('command-output');
+      outputTouch.textContent = 'Created file: ' + args[1];
+      terminalOutput.appendChild(outputTouch);
       break;
 
     case 'rm':
       // Implement logic to delete a file
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = 'Deleted file: ' + args[1];
-      terminalOutput.appendChild(output);
+      const outputRm = document.createElement('p');
+      outputRm.classList.add('command-output');
+      outputRm.textContent = 'Deleted file: ' + args[1];
+      terminalOutput.appendChild(outputRm);
       break;
 
     case 'pwd':
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = '/home/user'; // Replace with the actual current directory
-      terminalOutput.appendChild(output);
+      const outputPwd = document.createElement('p');
+      outputPwd.classList.add('command-output');
+      outputPwd.textContent = '/home/user'; // Replace with the actual current directory
+      terminalOutput.appendChild(outputPwd);
       break;
 
     case 'echo':
       const message = args.slice(1).join(' ');
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = message;
-      terminalOutput.appendChild(output);
+      const outputEcho = document.createElement('p');
+      outputEcho.classList.add('command-output');
+      outputEcho.textContent = message;
+      terminalOutput.appendChild(outputEcho);
       break;
 
     case 'cat':
       const fileToRead = args[1];
       // Implement logic to read and display the content of the file
       const fileContent = getFileContent(fileToRead); // Replace with the actual file reading logic
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = fileContent;
-      terminalOutput.appendChild(output);
+      const outputCat = document.createElement('p');
+      outputCat.classList.add('command-output');
+      outputCat.textContent = fileContent;
+      terminalOutput.appendChild(outputCat);
       break;
 
     case 'echo-date':
       const currentDate = new Date().toLocaleDateString();
-      const output = document.createElement('p');
-      output.classList.add('command-output');
-      output.textContent = currentDate;
-      terminalOutput.appendChild(output);
+      const outputEchoDate = document.createElement('p');
+      outputEchoDate.classList.add('command-output');
+      outputEchoDate.textContent = currentDate;
+      terminalOutput.appendChild(outputEchoDate);
       break;
 
     case 'help':
       if (args.length === 1) {
         const helpOutput = document.createElement('p');
         helpOutput.classList.add('command-output');
-        helpOutput.innerHTML = '<strong>Available commands:</strong><br>' + availableCommands.join('<br>');
-        terminalOutput.appendChild(helpOutput);
-      } else {
-        const output = document.createElement('p');
-        output.classList.add('command-output');
-        output.textContent = 'Help command usage: help';
-        terminalOutput.appendChild(output);
-      }
-      break;
-
-    default:
-      const
