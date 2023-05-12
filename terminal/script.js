6306f8734b83120b871963858359dd289584eeb6
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-12 09:04:03 -0500

    Update script.js

--- terminal/script.js
+++ terminal/script.js
@@ -3,24 +3,25 @@ const commandInput = document.getElementById('command-input');
 
 function processCommand(command) {
   const output = document.createElement('p');
+  output.classList.add('command-output');
   output.textContent = '> ' + command;
   terminalOutput.appendChild(output);
 
-  // Implement your command processing logic here
-  // This is just a sample command processing
   if (command === 'hello') {
     const response = document.createElement('p');
+    response.classList.add('command-output');
     response.textContent = 'Hello, there!';
     terminalOutput.appendChild(response);
   } else if (command === 'date') {
     const currentDate = new Date().toLocaleDateString();
     const response = document.createElement('p');
+    response.classList.add('command-output');
     response.textContent = 'Today is ' + currentDate;
     terminalOutput.appendChild(response);
   } else {
     const response = document.createElement('p');
+    response.classList.add('command-output', 'error');
     response.textContent = 'Invalid command: ' + command;
-    response.style.color = 'red';
     terminalOutput.appendChild(response);
   }
 }
@@ -29,6 +30,7 @@ commandInput.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     const command = commandInput.value;
     const output = document.createElement('p');
+    output.classList.add('command-output');
     output.textContent = '$ ' + command;
     terminalOutput.appendChild(output);
     processCommand(command);
