bb8375a509baaf07cf0a73571fe251578a216cba
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-16 09:11:32 -0500

    Update script.js

--- terminal/script.js
+++ terminal/script.js
@@ -97,201 +97,192 @@ document.addEventListener('DOMContentLoaded', function() {
   function getFileContent(file) {
     // Implement logic to read and return the content of the file
     // Replace with your own implementation
-    return 'File content of ' + file;
-  }
-
- function displayCommandOutput(output) {
-  const outputElement = document.createElement('p');
-  outputElement.classList.add('command-output');
-  outputElement.textContent = output;
-  terminalOutput.appendChild(outputElement);
-
-  // Scroll to the bottom of the output area
-  terminalOutput.scrollTop = terminalOutput.scrollHeight;
+return 'File content of ' + file;
 }
 
-  function displayCommandOutput(output) {
-  const outputElement = document.createElement('p');
-  outputElement.classList.add('command-output');
-  outputElement.textContent = output;
-  terminalOutput.appendChild(outputElement);
-
+function displayCommandOutput(output) {
+const outputElement = document.createElement('p');
+outputElement.classList.add('command-output');
+outputElement.textContent = output;
+terminalOutput.appendChild(outputElement);
   // Scroll to the bottom of the terminal output
-  terminalOutput.scrollTop = terminalOutput.scrollHeight;
+terminalOutput.scrollTop = terminalOutput.scrollHeight;
 }
 
-  function executeCommand(command) {
-    const args = command.split(' ');
+function executeCommand(command) {
+const args = command.split(' ');
+  switch (args[0]) {
+  case 'ls':
+    displayCommandOutput('File1.txt  File2.txt  Directory1');
+    break;
 
-    switch (args[0]) {
-      case 'ls':
-        displayCommandOutput('File1.txt  File2.txt  Directory1');
-        break;
-
-     case 'cd':
-  if (args.length > 1) {
-    // Implementation for 'cd' command
-    const directory = args[1];
-    displayCommandOutput(`Changed directory to ${directory}`);
-  } else {
-    displayCommandOutput('Missing directory name');
-  }
-  break;
-        case 'mkdir':
-        if (args.length > 1) {
-          // Implementation for 'mkdir' command
-          const directory = args[1];
-          displayCommandOutput(`Created directory: ${directory}`);
-        } else {
-          displayCommandOutput('Missing directory name');
-        }
-        break;
-
-      case 'touch':
-        if (args.length > 1) {
-          // Implementation for 'touch' command
-          const file = args[1];
-          displayCommandOutput(`Created file: ${file}`);
-        } else {
-          displayCommandOutput('Missing file name');
-        }
-        break;
-
-      case 'rm':
-        if (args.length > 1) {
-          // Implementation for 'rm' command
-          const file = args[1];
-          displayCommandOutput(`Deleted file: ${file}`);
-        } else {
-          displayCommandOutput('Missing file name');
-        }
-        break;
-
-      case 'pwd':
-        displayCommandOutput('/home/user'); // Replace with the actual current directory
-        break;
-
-      case 'echo':
-        if (args.length > 1) {
-          const message = args.slice(1).join(' ');
-          displayCommandOutput(message);
-        } else {
-          displayCommandOutput('Missing message');
-        }
-        break;
-
-      case 'cat':
-        if (args.length > 1) {
-          const fileToRead = args[1];
-          // Implement logic to read and display the content of the file
-          const fileContent = getFileContent(fileToRead); // Replace with the actual file reading logic
-          displayCommandOutput(fileContent);
-        } else {
-          displayCommandOutput('Missing file name');
-        }
-        break;
-
-      case 'echo-date':
-        const currentDate = new Date().toLocaleDateString();
-        displayCommandOutput(currentDate);
-        break;
-
-      case 'help':
-        displayHelp();
-        break;
-
-      case 'ip':
-        fetch('https://api.ipify.org?format=json')
-          .then(response => response.json())
-          .then(data => {
-            const ipOutput = `Your IP address is: ${data.ip}`;
-            displayCommandOutput(ipOutput);
-          })
-          .catch(error => {
-            displayCommandOutput('Failed to retrieve IP address');
-          });
-        break;
-
-      case 'clear':
-        clearTerminal();
-        break;
-
-      case 'sudo':
-        if (args.length > 1) {
-          const sudoCommand = args.slice(1).join(' ');
-          displayCommandOutput(`Running command '${sudoCommand}' with administrative privileges`);
-        } else {
-          displayCommandOutput('Missing command after sudo');
-        }
-        break;
-
-      case 'top':
-        displayCommandOutput('System resource usage:');
-        // Implement logic to display system resource usage
-        break;
-
-      case 'exit':
-        // Terminate the terminal
-        break;
-
-      case 'sl':
-        displayCommandOutput('Command not available');
-        break;
-
-      case 'cowsay':
-        if (args.length > 1) {
-          const message = args.slice(1).join(' ');
-          const cowSaid = `  ${'\\'.repeat(message.length + 2)}\n< ${message} >\n  ${'/'.repeat(message.length + 2)}`;
-          const cowOutput = `<pre>${cowSaid}</pre>`;
-          displayCommandOutput(cowOutput);
-        } else {
-          displayCommandOutput('Missing message');
-        }
-        break;
-        case 'calc':
-  if (args.length > 1) {
-    const expression = args.slice(1).join(' ');
-    try {
-      const result = eval(expression);
-      displayCommandOutput(`Result: ${result}`);
-    } catch (error) {
-      displayCommandOutput('Invalid expression');
+  case 'cd':
+    if (args.length > 1) {
+      // Implementation for 'cd' command
+      const directory = args[1];
+      displayCommandOutput(`Changed directory to ${directory}`);
+    } else {
+      displayCommandOutput('Missing directory name');
     }
-  } else {
-    displayCommandOutput('Missing expression');
-  }
-  break;
-case 'time':
-        const currentTime = new Date().toLocaleTimeString();
-        displayCommandOutput(currentTime);
-        break;
+    break;
 
-      case 'download':
-        if (args.length > 2) {
-          const url = args[1];
-          const filename = args[2];
-          downloadFile(url, filename);
-        } else {
-          displayCommandOutput('Missing URL or filename');
-        }
-        break;
-
-      default:
-        displayCommandOutput(`Command not found: ${args[0]}`);
-        break;
+  case 'mkdir':
+    if (args.length > 1) {
+      // Implementation for 'mkdir' command
+      const directory = args[1];
+      displayCommandOutput(`Created directory: ${directory}`);
+    } else {
+      displayCommandOutput('Missing directory name');
     }
-  }
+    break;
 
-  function downloadFile(url, filename) {
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = filename;
-    link.target = '_blank';
-    link.click();
-    displayCommandOutput(`Downloading file: ${filename}`);
-  }
+  case 'touch':
+    if (args.length > 1) {
+      // Implementation for 'touch' command
+      const file = args[1];
+      displayCommandOutput(`Created file: ${file}`);
+    } else {
+      displayCommandOutput('Missing file name');
+    }
+    break;
 
-  function clearTerminal() {
-    terminalOutput.innerHTML = '';
-  }
+  case 'rm':
+    if (args.length > 1) {
+      // Implementation for 'rm' command
+      const file = args[1];
+      displayCommandOutput(`Deleted file: ${file}`);
+    } else {
+      displayCommandOutput('Missing file name');
+    }
+    break;
+
+  case 'pwd':
+    displayCommandOutput('/home/user'); // Replace with the actual current directory
+    break;
+
+  case 'echo':
+    if (args.length > 1) {
+      const message = args.slice(1).join(' ');
+      displayCommandOutput(message);
+    } else {
+      displayCommandOutput('Missing message');
+    }
+    break;
+
+  case 'cat':
+    if (args.length > 1) {
+      const fileToRead = args[1];
+      // Implement logic to read and display the content of the file
+      const fileContent = getFileContent(fileToRead); // Replace with the actual file reading logic
+      displayCommandOutput(fileContent);
+    } else {
+      displayCommandOutput('Missing file name');
+    }
+    break;
+
+  case 'echo-date':
+    const currentDate = new Date().toLocaleDateString();
+    displayCommandOutput(currentDate);
+    break;
+
+  case 'help':
+    displayHelp();
+    break;
+
+  case 'ip':
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+        const ipOutput = `Your IP address is: ${data.ip}`;
+        displayCommandOutput(ipOutput);
+      })
+      .catch(error => {
+        displayCommandOutput('Failed to retrieve IP address');
+      });
+    break;
+
+  case 'clear':
+    clearTerminal();
+    break;
+
+  case 'sudo':
+    if (args.length > 1) {
+      const sudoCommand = args.slice(1).join(' ');
+      displayCommandOutput(`Running command '${sudoCommand}' with administrative privileges`);
+    } else {
+      displayCommandOutput('Missing command after sudo');
+    }
+    break;
+
+  case 'top':
+    displayCommandOutput('System resource usage:');
+    // Implement logic to display system resource usage
+    break;
+
+  case 'exit':
+    // Terminate the terminal
+    break;
+
+  case 'sl':
+    displayCommandOutput('Command
+not available');
+break;
+                           case 'cowsay':
+    if (args.length > 1) {
+      const message = args.slice(1).join(' ');
+      const cowSaid = `  ${'\\'.repeat(message.length + 2)}\n< ${message} >\n  ${'/'.repeat(message.length + 2)}`;
+      const cowOutput = `<pre>${cowSaid}</pre>`;
+      displayCommandOutput(cowOutput);
+    } else {
+      displayCommandOutput('Missing message');
+    }
+    break;
+
+  case 'calc':
+    if (args.length > 1) {
+      const expression = args.slice(1).join(' ');
+      try {
+        const result = eval(expression);
+        displayCommandOutput(`Result: ${result}`);
+      } catch (error) {
+        displayCommandOutput('Invalid expression');
+      }
+    } else {
+      displayCommandOutput('Missing expression');
+    }
+    break;
+
+  case 'time':
+    const currentTime = new Date().toLocaleTimeString();
+    displayCommandOutput(currentTime);
+    break;
+
+  case 'download':
+    if (args.length > 2) {
+      const url = args[1];
+      const filename = args[2];
+      downloadFile(url, filename);
+    } else {
+      displayCommandOutput('Missing URL or filename');
+    }
+    break;
+
+  default:
+    displayCommandOutput(`Command not found: ${args[0]}`);
+    break;
+}
+}
+
+function downloadFile(url, filename) {
+const link = document.createElement('a');
+link.href = url;
+link.download = filename;
+link.target = '_blank';
+link.click();
+displayCommandOutput(Downloading file: ${filename});
+}
+
+function clearTerminal() {
+terminalOutput.innerHTML = '';
+}
 });
