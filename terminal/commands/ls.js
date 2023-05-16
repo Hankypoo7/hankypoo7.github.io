245677122214d97d7c44cb36ba5cc8a96d98948c
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-16 12:56:22 -0500

    Create ls.js

---
+++ terminal/commands/ls.js
@@ -0,0 +1,10 @@
+// commands/ls.js
+
+module.exports = {
+  command: 'ls',
+  description: 'List files and directories',
+  execute: function () {
+    // Add your implementation for the 'ls' command here
+    console.log('Executing ls command');
+  }
+};
