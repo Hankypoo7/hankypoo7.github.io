20b09b0b744dd05f78c3f52c9e74a3ddbd1e3976
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-16 12:58:01 -0500

    Create cd.js

---
+++ terminal/commands/cd.js
@@ -0,0 +1,10 @@
+// commands/cd.js
+
+module.exports = {
+  command: 'cd',
+  description: 'Change directory',
+  execute: function () {
+    // Add your implementation for the 'cd' command here
+    console.log('Executing cd command');
+  }
+};
